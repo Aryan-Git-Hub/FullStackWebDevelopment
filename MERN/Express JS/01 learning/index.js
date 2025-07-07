f7a606e8c21899aba71b278c8164d94a7f984ffb
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-07-07 14:01:37 +0530

    custom middleware done

--- MERN/Express JS/01 learning/index.js
+++ MERN/Express JS/01 learning/index.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+function customMiddleware(req, res, next) {
+  console.log("Custom middleware executed");
+  console.log(`Request Method: ${req.method}`);
+  next(); // Pass control to the next middleware or route handler
+  console.log("Custom middleware completed");
+}
+app.use(customMiddleware);
 
 app.get('/', (req, res) => {
+  console.log("Request received at root endpoint");
   res.send('Hello, World!');
 });
 
 app.post('/submit', (req, res) => {
   const { name, email } = req.body;
-  console.log(typeof(req.body));
+  console.log(typeof(req.body), req.body);
   res.send(`Received submission: Name - ${name}, Email - ${email}`);
 });
 
